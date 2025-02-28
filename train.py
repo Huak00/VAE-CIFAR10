2f195ebe4c196ae6ffb23062193218dd80aaf5b6
Huak00 <huak7066581@gmail.com>
2025-02-28 17:27:52 -0500

    Update train.py

--- train.py
+++ train.py
@@ -1,1 +1,17 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+
+# 加载数据
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True)
+
+print("Data loaded successfully!")
 
